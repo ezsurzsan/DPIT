688e6a7946c71529c5df3d88dedacc62fb250430
Macovei Tudor <tudor_macovei@iCloud.com>
2019-09-21 08:45:16 +0300

    Fix open_now dis[;ay status of places

--- frontend/clujlive/src/components/InfoWindowContent.js
+++ frontend/clujlive/src/components/InfoWindowContent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import './InfoWindowContent.css';
 
 class InfoWindowContent extends Component {
-  isOpen(open) {
+  openText(open) {
     if (open) {
       return "Deschis";
     } else {
@@ -10,17 +10,25 @@ class InfoWindowContent extends Component {
     }
   }
 
+  openColor(open) {
+    if (open) {
+      return "#4bd162";
+    } else {
+      return "#000000";
+    }
+  }
+
   render() {
     return (
-      <div style={{ width:'310px', overflow: 'hidden'}}>
+      <div style={{ width: '310px', overflow: 'hidden' }}>
         <h2 style={{ fontFamily: 'Roboto', textAlign: 'center' }}>{this.props.place.name}</h2>
-        <p style={{ textAlign: 'center', fontSize:'120%', color: '#4bd162', fontFamily: 'Roboto' }}><b>{this.isOpen(this.props.place.details.open_now)}</b></p>
-        <p> 
-        <div class="btn-group btn-group-justified">
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={ this.props.place.menuLink } target={'_blank'}>Meniu</a> 
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={ this.props.place.website } target={'_blank'}>Website</a> 
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={ this.props.place.reservationLink } target="_blank">Rezervari</a>
-        </div>
+        <p style={{ textAlign: 'center', fontSize: '120%', color: this.openColor(this.props.place.details.open_now[0]), fontFamily: 'Roboto' }}><b>{this.openText(this.props.place.details.open_now[0])}</b></p>
+        <p>
+          <div class="btn-group btn-group-justified">
+            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.menuLink} target={'_blank'}>Meniu</a>
+            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.website} target={'_blank'}>Website</a>
+            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.reservationLink} target="_blank">Rezervari</a>
+          </div>
         </p>
       </div>
     )
