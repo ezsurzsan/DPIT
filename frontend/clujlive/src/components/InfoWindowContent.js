c22d6be293f79b3c6604d2042bcdaa48d53d1ddd
andreeam18 <murariu.andreea18@gmail.com>
2019-09-26 22:53:21 +0300

    Modify InfoWindow buttons

--- frontend/clujlive/src/components/InfoWindowContent.js
+++ frontend/clujlive/src/components/InfoWindowContent.js
@@ -14,7 +14,7 @@ class InfoWindowContent extends Component {
     if (open) {
       return "#4bd162";
     } else {
-      return "#000000";
+      return "#e32019";
     }
   }
 
@@ -25,20 +25,20 @@ class InfoWindowContent extends Component {
 
   render() {
     return (
-      <div style={{ width: '310px', overflow: 'hidden' }}>
+      <div style={{ width: '320px', overflow: 'hidden' }}>
         <h2 style={{ fontFamily: 'Roboto', textAlign: 'center' }}>{this.props.place.name}</h2>
         <div style={{ width: '85%', margin: '0 auto' }}>
           <p style={{ float: 'left', textAlign: 'center', fontSize: '120%', color: this.openColor(this.props.place.details.open_now[0]), fontFamily: 'Roboto' }}><b>{this.openText(this.props.place.details.open_now[0])}</b></p>
           <p style={{ float: 'right', textAlign: 'center', fontSize: '120%', color: '#ffdf00', fontFamily: 'Roboto', fontWeight: '900' }}>
             Rating: {this.props.place.details.rating}
           </p>
-          <button onClick={() => this.handleClick(this.props.place)}></button>
         </div>
         <p>
           <div class="btn-group btn-group-justified">
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.menuLink} target={'_blank'}>Meniu</a>
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.website} target={'_blank'}>Website</a>
-            <a class="btn btn-info" role="button" style={{ textAlign: 'center' }} href={this.props.place.reservationLink} target="_blank">Rezervari</a>
+          <a class="btn btn-info" role="button" style={{ textAlign: 'center', fontSize: '90%' }} href={this.props.place.menuLink} target={'_blank'}>Menu</a>
+          <button type="button" className="btn btn-info" style={{ textAlign: 'center', fontSize: '90%'}} onClick={() => this.handleClick(this.props.place)}>Directions</button>
+          <a class="btn btn-info" role="button" style={{ textAlign: 'center', fontSize: '90%'}} href={this.props.place.reservationLink} target="_blank">Reservations</a>
+          <a class="btn btn-info" role="button" style={{ textAlign: 'center', fontSize: '90%' }} href={this.props.place.website} target={'_blank'}>Website</a>
           </div>
         </p>
       </div>
